a18dfeb34b3a3b8978d8ccc6dcf248434445fbfe
rogerujn <48398137+rogerujn@users.noreply.github.com>
2026-02-28 18:44:29 +0800

    Update GitHub link in Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -8,7 +8,7 @@ sed -i "s#_('Firmware Version'), (L\.isObject(boardinfo\.release) ? boardinfo\.r
                 ? boardinfo.release.description + ' / '\n \
                 : '') + (luciversion || '') + ' / ',\n \
             E('a', {\n \
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',\n \
+                href: 'https://github.com/rogerujn/openwrt-ci-roc/releases',\n \
                 target: '_blank',\n \
                 rel: 'noopener noreferrer'\n \
                 }, [ 'Built by Rogerujn $(date "+%Y-%m-%d %H:%M:%S")' ])\n \
